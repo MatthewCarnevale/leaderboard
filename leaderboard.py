78505c38498b64d53cd3a897f97fd0f616f60a34
Marty <matthewcarnevale1@gmail.com>
2020-12-25 03:15:47 -0500

    done for the night read text doc for speficis

--- leaderboard.py
+++ leaderboard.py
@@ -6,6 +6,9 @@ import psycopg2
 import datetime
 from os.path import join, dirname
 from dotenv import load_dotenv
+import enum
+from timer import daily, hourly
+import time
 
 dotenv_path = join(dirname(__file__), "keys.env")
 load_dotenv(dotenv_path)
@@ -15,39 +18,87 @@ riot = os.environ["RIOT"]
 
 lolwatcher = LolWatcher(riot)
 my_region="na1"
-#account specific data
-#full user list, currently limited by api call amount
-#users = ["MarTea", "StinGod", "Bassel", "Trúst", "Big ItzWeird", "K3v1nRul3s", "Kareem100", "AminRhino", "Mama Zer0", "Xerous", "Vayler", "Glorious Duelist", "Godric II", "Shadowninjas13", "Kalichi", "Riko Best Girl", "Jebal", "Jin Vi", "KerØ"]
-users = ["MarTea", "StinGod", "Bassel", "Trúst"]
+users = ["MarTea", "StinGod", "Bassel", "Trúst", "Big ItzWeird", "K3v1nRul3s", "Kareem100", "AminRhino", "Mama Zer0", "Xerous", "Vayler", "Glorious Duelist", "Godric II", "Shadowninjas13", "Kalichi", "Riko Best Girl", "Jebal", "Jin Vi", "KerØ"]
+#users = ["MarTea"]
 length = len(users)
 nameList, levelList, tierList, rankList, lpList, winsList, loseList = [], [], [], [], [], [], []
-for user in users:
-    queueID = 0
-    user = lolwatcher.summoner.by_name(my_region, user)
-    ranked_stats = lolwatcher.league.by_summoner(my_region, user['id'])
-    queue = ranked_stats[queueID].get("queueType")
-    if queue == "RANKED_SOLO_5x5":
+def rankedUpdate():
+    for user in users:
         queueID = 0
-    else:
-        queueID = 1
-    nameList.append(user.get("name"))
-    levelList.append(user.get("summonerLevel"))
-    tierList.append(ranked_stats[queueID].get("tier").lower().capitalize())
-    rankList.append(ranked_stats[queueID].get("rank"))
-    lpList.append(ranked_stats[queueID].get("leaguePoints"))
-    winsList.append(ranked_stats[queueID].get("wins"))
-    loseList.append(ranked_stats[queueID].get("losses"))
-    #print(queueID)
-    #print(name)
-    #print(level)
-    #print(tier)
-    #print(rank)
-    #print(lp)
-    #print(wins)
-    #print(lose)
+        user = lolwatcher.summoner.by_name(my_region, user)
+        ranked_stats = lolwatcher.league.by_summoner(my_region, user['id'])
+        queue = ranked_stats[queueID].get("queueType")
+        if queue == "RANKED_SOLO_5x5":
+            queueID = 0
+        else:
+            queueID = 1
+        nameList.append(user.get("name"))
+        levelList.append(user.get("summonerLevel"))
+        tierList.append(ranked_stats[queueID].get("tier").lower().capitalize())
+        rankList.append(ranked_stats[queueID].get("rank"))
+        lpList.append(ranked_stats[queueID].get("leaguePoints"))
+        winsList.append(ranked_stats[queueID].get("wins"))
+        loseList.append(ranked_stats[queueID].get("losses"))
+        #print(queueID)
+        #print(name)
+        #print(level)
+        #print(tier)
+        #print(rank)
+        #print(lp)
+        #print(wins)
+        #print(lose)
+
+class Ranks(enum.Enum):
+    Iron = 0
+    Bronze = 1
+    Silver = 2
+    Gold = 3
+    Platinum = 4
+    Diamond = 5
+    Master = 6
+    Grandmaster = 7
+    Challenger = 8
+#for rank in (Ranks):
+#    print(rank)
+testRank = Ranks.Platinum
+#print("the enum associated with " + testRank.name + " is " + str(testRank.value))
+
+def dbCon():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="leaderboard",
+        user="postgres",
+        password="password")
+    cur = conn.cursor()
+    return conn, cur
+
+def timeTest():
+    conn, cur = dbCon()
+    date = datetime.datetime.now()
+    hour = int(date.strftime("%H"))
+    date = date.strftime("%x")
+    oldDateFetch = "SELECT date FROM timetracker ORDER BY id DESC LIMIT 1;"
+    cur.execute(oldDateFetch)
+    oldDate = cur.fetchone()
+    oldHourFetch = "SELECT hour FROM timetracker ORDER BY id DESC LIMIT 1;"
+    cur.execute(oldHourFetch)
+    oldHour = cur.fetchone()
+    try:
+        if date != oldDate[0] or hour != oldHour[0]:
+            sql = "INSERT INTO timetracker(date, hour) VALUES (%s,%s);"
+            cur.execute(sql, (date, hour))
+            conn.commit()
+    except:
+        sql = "INSERT INTO timetracker(date, hour) VALUES (%s,%s);"
+        cur.execute(sql, (date, hour))
+        conn.commit()
+    cur.close()
+    conn.close()
 
 @app.route("/")
 def index():
+    timeTest()
+    rankedUpdate()
     return flask.render_template(
         "index.html",
         nameList=nameList,
--- timer.py
+++ timer.py
@@ -3,9 +3,73 @@ import datetime
 from datetime import timedelta
 import schedule
 import time
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from riotwatcher import LolWatcher, ApiError
+"""
+dotenv_path = join(dirname(__file__), "keys.env")
+load_dotenv(dotenv_path)
+
+riot = os.environ["RIOT"]
+
+lolwatcher = LolWatcher(riot)
+my_region="na1"
 
 date = datetime.datetime.now()
-print(date)
+users = ["MarTea"]
+def daily():
+    for user in users:
+        user = lolwatcher.summoner.by_name(my_region, user)
+        #matches = lolwatcher.match.matchlist_by_account(my_region, user['accountId'])
+        #print(matches)
+        #last_match = matches['matches'][0]
+        #if(last_match['queue'] != 420):
+        #    continue
+        #print(last_match)
+        #match_detail = lolwatcher.match.by_id(my_region, last_match['gameId'])
+        #print(match_detail)
+
+
+
+
+
+daily()
+
+
+"""
+
+dotenv_path = join(dirname(__file__), "keys.env")
+load_dotenv(dotenv_path)
+
+riot = os.environ["RIOT"]
+
+lolwatcher = LolWatcher(riot)
+my_region="na1"
+
+date = datetime.datetime.now()
+
+users = ["MarTea"]
+def hourly():
+    queueID = 0
+    for user in users:
+        user = lolwatcher.summoner.by_name(my_region, user)
+        ranked_stats = lolwatcher.league.by_summoner(my_region, user['id'])
+        queue = ranked_stats[queueID].get("queueType")
+        if queue == "RANKED_SOLO_5x5":
+            queueID = 0
+        else:
+            queueID = 1
+        nameList.append(user.get("name"))
+        levelList.append(user.get("summonerLevel"))
+        tierList.append(ranked_stats[queueID].get("tier").lower().capitalize())
+        rankList.append(ranked_stats[queueID].get("rank"))
+        lpList.append(ranked_stats[queueID].get("leaguePoints"))
+        winsList.append(ranked_stats[queueID].get("wins"))
+        loseList.append(ranked_stats[queueID].get("losses"))
+
+#schedule.every().hour.do(hourly)
+
 def daily():
     conn = psycopg2.connect(
         host="localhost",
@@ -27,8 +91,8 @@ def daily():
     cur.close()
     conn.close()
 
-schedule.every().day.at("01:00").do(daily)
+#schedule.every().day.at("01:00").do(daily)
 
-while True:
-    schedule.run_pending()
-    time.sleep(60) # checks every minute to see if the time is the right time
+#while True:
+#    schedule.run_pending()
+#    time.sleep(60) # checks every minute to see if the time is the right time
