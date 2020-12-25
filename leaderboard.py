2074e8194256b6bd3e552c71582ef534422998ed
Marty <matthewcarnevale1@gmail.com>
2020-12-25 18:53:26 -0500

    kalichi told me to make player class and use a dict so i did D:

--- leaderboard.py
+++ leaderboard.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 import enum
 from timer import daily, hourly
 import time
+from player import Player
 
 dotenv_path = join(dirname(__file__), "keys.env")
 load_dotenv(dotenv_path)
@@ -18,50 +19,6 @@ riot = os.environ["RIOT"]
 
 lolwatcher = LolWatcher(riot)
 my_region="na1"
-users = ["MarTea", "StinGod", "Bassel", "Trúst", "Big ItzWeird", "K3v1nRul3s", "Kareem100", "AminRhino", "Mama Zer0", "Xerous", "Vayler", "Glorious Duelist", "Godric II", "Shadowninjas13", "Kalichi", "Riko Best Girl", "Jebal", "Jin Vi", "KerØ"]
-#users = ["MarTea"]
-length = len(users)
-nameList, levelList, tierList, rankList, lpList, winsList, loseList = [], [], [], [], [], [], []
-def rankedUpdate():
-    for user in users:
-        queueID = 0
-        user = lolwatcher.summoner.by_name(my_region, user)
-        ranked_stats = lolwatcher.league.by_summoner(my_region, user['id'])
-        queue = ranked_stats[queueID].get("queueType")
-        if queue == "RANKED_SOLO_5x5":
-            queueID = 0
-        else:
-            queueID = 1
-        nameList.append(user.get("name"))
-        levelList.append(user.get("summonerLevel"))
-        tierList.append(ranked_stats[queueID].get("tier").lower().capitalize())
-        rankList.append(ranked_stats[queueID].get("rank"))
-        lpList.append(ranked_stats[queueID].get("leaguePoints"))
-        winsList.append(ranked_stats[queueID].get("wins"))
-        loseList.append(ranked_stats[queueID].get("losses"))
-        #print(queueID)
-        #print(name)
-        #print(level)
-        #print(tier)
-        #print(rank)
-        #print(lp)
-        #print(wins)
-        #print(lose)
-
-class Ranks(enum.Enum):
-    Iron = 0
-    Bronze = 1
-    Silver = 2
-    Gold = 3
-    Platinum = 4
-    Diamond = 5
-    Master = 6
-    Grandmaster = 7
-    Challenger = 8
-#for rank in (Ranks):
-#    print(rank)
-testRank = Ranks.Platinum
-#print("the enum associated with " + testRank.name + " is " + str(testRank.value))
 
 def dbCon():
     conn = psycopg2.connect(
@@ -72,10 +29,39 @@ def dbCon():
     cur = conn.cursor()
     return conn, cur
 
+#users = ["MarTea", "StinGod", "Bassel", "Trúst", "Big ItzWeird", "K3v1nRul3s", "Kareem100", "AminRhino", "Mama Zer0", "Xerous", "Vayler", "Glorious Duelist", "Godric II", "Shadowninjas13", "Kalichi", "Riko Best Girl", "Jebal", "Jin Vi", "KerØ"]
+users = ["MarTea", "StinGod"]
+
+def playerCreate():
+    playerList = []
+    for user in users:
+        queueID = 0
+        summoner = lolwatcher.summoner.by_name(my_region, user)
+        ranked_stats = lolwatcher.league.by_summoner(my_region, summoner['id'])
+        queue = ranked_stats[queueID].get("queueType")
+        if queue == "RANKED_SOLO_5x5":
+            queueID = 0
+        else:
+            queueID = 1
+        player =  Player(summoner.get("name"), summoner.get("summonerLevel"), ranked_stats[queueID].get("tier").lower().capitalize(), ranked_stats[queueID].get("rank"),ranked_stats[queueID].get("leaguePoints"),ranked_stats[queueID].get("wins"), ranked_stats[queueID].get("losses"))
+        playerList.append(player)
+    length = len(playerList)
+    return playerList, length
+players, length = playerCreate()
+
+def constructDict(playerList):
+    playerDict = {}
+    for player in players:
+        playerDict[player.name] = [player.level, player.tier, player.rank, player.lp, player.wins, player.losses]
+    return playerDict
+print(constructDict(players))
+
 def timeTest():
+    playerList = playerCreate()
     conn, cur = dbCon()
     date = datetime.datetime.now()
     hour = int(date.strftime("%H"))
+    minutes = int(date.strftime("%M"))
     date = date.strftime("%x")
     oldDateFetch = "SELECT date FROM timetracker ORDER BY id DESC LIMIT 1;"
     cur.execute(oldDateFetch)
@@ -83,58 +69,36 @@ def timeTest():
     oldHourFetch = "SELECT hour FROM timetracker ORDER BY id DESC LIMIT 1;"
     cur.execute(oldHourFetch)
     oldHour = cur.fetchone()
+    oldMinuteFetch = "SELECT minutes FROM timetracker ORDER BY id DESC LIMIT 1;"
+    cur.execute(oldMinuteFetch)
+    oldMinute = cur.fetchone()
+    #checks for timeing, updates every 30 minutes or sooner. right now just tracks time accurately, under the if should store all player data into database, add an else to load that data rather than str8 api pull
     try:
-        if date != oldDate[0] or hour != oldHour[0]:
-            sql = "INSERT INTO timetracker(date, hour) VALUES (%s,%s);"
-            cur.execute(sql, (date, hour))
+        if date != oldDate[0] or hour != oldHour[0] or (oldMinute[0] <= 30 and minutes > 30) :
+            sql = "INSERT INTO timetracker(date, hour, minutes) VALUES (%s,%s,%s);"
+            cur.execute(sql, (date, hour, minutes))
             conn.commit()
+            playerDict = constructDict(playerList)
+            #gonna try and make dictonary like good boy instead of list otp
+            #sql = "INSERT INTO playerdata()"
+        #TEMP TEMP TEMP TEMP TEMP BELOW TEMP TEMP TEMP TEMP
+        playerDict = constructDict(playerList)
     except:
-        sql = "INSERT INTO timetracker(date, hour) VALUES (%s,%s);"
-        cur.execute(sql, (date, hour))
+        sql = "INSERT INTO timetracker(date, hour, minutes) VALUES (%s,%s,%s);"
+        cur.execute(sql, (date, hour, minutes))
         conn.commit()
+        #TEMP TEMP TEMP TEMP TEMP BELOW TEMP TEMP TEMP TEMP
+        playerDict = constructDict(playerList)
     cur.close()
     conn.close()
+    return playerDict
 
 @app.route("/")
 def index():
-    timeTest()
-    rankedUpdate()
+    playerDict = timeTest()
     return flask.render_template(
         "index.html",
-        nameList=nameList,
-        levelList=levelList,
-        tierList=tierList,
-        rankList=rankList,
-        lpList=lpList,
-        winsList=winsList,
-        loseList=loseList,
-        length=length
+        playerDict = playerDict
     )
 
 app.run(port=int(os.getenv("PORT", 8080)), host=os.getenv("IP", "0.0.0.0"))
-
-
-
-
-
-"""    
-me = lolwatcher.summoner.by_name(my_region, "Stin God")
-#ranked data, seperated into flex and solo for the account
-my_ranked_stats = lolwatcher.league.by_summoner(my_region, me['id'])
-print(my_ranked_stats)
-name = me.get("name")
-level = me.get("summonerLevel")
-tier = my_ranked_stats[0].get("tier") 
-rank = my_ranked_stats[0].get("rank")
-lp = my_ranked_stats[0].get("leaguePoints")
-wins = my_ranked_stats[0].get("wins")
-lose = my_ranked_stats[0].get("losses")
-#test print statements to make sure things work
-print(name)
-print(level)
-print(tier)
-print(rank)
-print(lp)
-print(wins)
-print(lose)
-"""
---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, name, level, tier, rank, lp, wins, losses):
+        self.name = name
+        self.level = level
+        self.tier = tier
+        self.rank = rank
+        self.lp = lp
+        self.wins = wins
+        self.losses = losses
