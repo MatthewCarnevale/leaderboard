2bcf293b98600d28cfa9fa4c632be67ac515ecb7
Marty <matthewcarnevale1@gmail.com>
2020-12-28 18:12:22 -0500

    removed time checking if clause, commented out

--- timer.py
+++ timer.py
@@ -21,37 +21,37 @@ def timeTest():
     oldMinute = cur.fetchone()
     #try:
     #needs a smaller miunte window, probably 20~. if a real api key makes things faster i can do even smaller intervals (10 is optimal), if a real api key is the same we wont as large a window as possible without losing game tracking
-    if date != oldDate[0] or hour != oldHour[0] or (oldMinute[0] <= 20 and minutes > 20) or (oldMinute[0] <= 40 and minutes > 40) or 1==1:
-        print("doin the update")
-        players = playerCreate()
-        playerDict = constructDict(players)
-        if date != oldDate[0]:
-            print("making daily lp check")
-            sql = "INSERT INTO dailylp (summoner, date, lp, totalgames) VALUES (%s,%s,%s,%s);"
-            for key, value in playerDict.items():
-                totalGames = value[7] + value[8]
-                cur.execute(sql, (key, date, value[4], totalGames))
-                conn.commit()
-        sql = "INSERT INTO timetracker(date, hour, minutes) VALUES (%s,%s,%s);"
-        cur.execute(sql, (date, hour, minutes))
-        conn.commit()
-        #get lp from playerDict, compare it to select query from dailylp lp value, insert new value as lpdelta
-        counter = 0
+    #if date != oldDate[0] or hour != oldHour[0] or (oldMinute[0] <= 20 and minutes > 20) or (oldMinute[0] <= 40 and minutes > 40):
+    print("doin the update")
+    players = playerCreate()
+    playerDict = constructDict(players)
+    if date != oldDate[0]:
+        print("making daily lp check")
+        sql = "INSERT INTO dailylp (summoner, date, lp, totalgames) VALUES (%s,%s,%s,%s);"
         for key, value in playerDict.items():
-            sql = "SELECT lp FROM dailylp ORDER BY id DESC LIMIT 19"
-            cur.execute(sql)
-            dailyLP = cur.fetchall()
-            dailyLP.reverse()
-            startingMmr = dailyLP[counter][0]
-            sql = "SELECT totalgames FROM dailylp ORDER BY id DESC LIMIT 19"
-            cur.execute(sql)
-            dailyGames = cur.fetchall()
-            dailyGames.reverse()
-            totalDayGames = (value[7] + value[8]) - dailyGames[counter][0]
-            counter = counter+1
-            delta = value[4] - startingMmr
-            sql = "INSERT INTO playerdata(name,level,tier,rank,lp, mmr, lpdelta, dailygames, wins,losses) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-            cur.execute(sql, (key, value[0],value[1],value[2],value[3],value[4], delta, totalDayGames, value[7],value[8]))
+            totalGames = value[7] + value[8]
+            cur.execute(sql, (key, date, value[4], totalGames))
             conn.commit()
+    sql = "INSERT INTO timetracker(date, hour, minutes) VALUES (%s,%s,%s);"
+    cur.execute(sql, (date, hour, minutes))
+    conn.commit()
+    #get lp from playerDict, compare it to select query from dailylp lp value, insert new value as lpdelta
+    counter = 0
+    for key, value in playerDict.items():
+        sql = "SELECT lp FROM dailylp ORDER BY id DESC LIMIT 19"
+        cur.execute(sql)
+        dailyLP = cur.fetchall()
+        dailyLP.reverse()
+        startingMmr = dailyLP[counter][0]
+        sql = "SELECT totalgames FROM dailylp ORDER BY id DESC LIMIT 19"
+        cur.execute(sql)
+        dailyGames = cur.fetchall()
+        dailyGames.reverse()
+        totalDayGames = (value[7] + value[8]) - dailyGames[counter][0]
+        counter = counter+1
+        delta = value[4] - startingMmr
+        sql = "INSERT INTO playerdata(name,level,tier,rank,lp, mmr, lpdelta, dailygames, wins,losses) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+        cur.execute(sql, (key, value[0],value[1],value[2],value[3],value[4], delta, totalDayGames, value[7],value[8]))
+        conn.commit()
 timeTest()
 print("ok me done with api push")
